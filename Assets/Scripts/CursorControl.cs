852c1c963bcb67304dd97660b1b33db968034caf
DekhanFraser <alexander.izmaylov@gmail.com>
2017-04-05 13:18:10 +1200

    Cursor movement and unit spawning test

---
+++ Assets/Scripts/CursorControl.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorControl : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.A))
+        {
+            Debug.Log("A pressed");
+            Vector3 position = gameObject.transform.position;
+            position.x--;
+            gameObject.transform.position = position;
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            Debug.Log("D pressed");
+            Vector3 position = gameObject.transform.position;
+            position.x++;
+            gameObject.transform.position = position;
+        }
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            Debug.Log("W pressed");
+            Vector3 position = gameObject.transform.position;
+            position.z++;
+            gameObject.transform.position = position;
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            Debug.Log("S pressed");
+            Vector3 position = gameObject.transform.position;
+            position.z--;
+            gameObject.transform.position = position;
+        }
+    }
+}
