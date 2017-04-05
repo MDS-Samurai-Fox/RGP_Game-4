f9d3b6f519e36d4175b044609cd6c0498aaba8cb
mitchellcurrie <mitchell.jc@hotmail.com>
2017-04-05 23:10:47 +1200

    Cat seeking nearest dog and attacking

--- Assets/Scripts/Cat.cs
+++ Assets/Scripts/Cat.cs
@@ -10,20 +10,26 @@ public class Cat : MonoBehaviour {
     public float speed = 0.1f;
     public float health = 100;
     public float damageDealt = 1;
-    public float dttackFrequency = 1;
+    public float attackFrequency = 1.0f;
+
+    private Dog[] DogArray;
+    private float attackTimer;
 
 
     // Use this for initialization
     void Start() {
 
         //Velocity *= speed;
+        Velocity = new Vector3(speed, 0.0f, 0.0f);
 
     }
 
     // Update is called once per frame
     void FixedUpdate() {
 
-        transform.position += Velocity * speed;
+        //transform.position += Velocity * speed;
+        transform.position += Velocity;
+        FindNearestDog();
         CheckCollision();
 
     }
@@ -34,18 +40,59 @@ public class Cat : MonoBehaviour {
 
         if (Physics.Raycast(transform.position, transform.forward, out hit, 0.6f, dogCollisionMask)) {
 
-            Velocity = new Vector3(0.0f, 0.0f, 0.0f);
+            Velocity = Vector3.zero;
+           // print("hit");
 
             Dog DefenderDog = hit.transform.GetComponent<Dog>();
 
-            if (DefenderDog) {
+            if (DefenderDog)
+            {
+                attackTimer += Time.deltaTime;
 
-                DefenderDog.TakeDamage(damageDealt);
+                if (attackTimer >= attackFrequency)
+                {
+                    attackTimer = 0;
+                    DefenderDog.TakeDamage(damageDealt);
+                }
 
             }
 
         }
 
+    }
+
+    private void FindNearestDog()
+    {
+        DogArray = FindObjectsOfType<Dog>();
+        float distance = 10000.0f;
+        Vector3 ClosestDogPosition = Vector3.zero;
+
+        if (DogArray.Length > 0)
+        {
+            foreach (Dog dog in DogArray)
+            {
+                float tempDistance = Vector3.Magnitude(dog.transform.position - gameObject.transform.position);
+
+                if (tempDistance < distance)
+                {
+                    distance = tempDistance;
+                    ClosestDogPosition = dog.transform.position;
+                }
+            }
+
+            Vector3 DesiredVelocity = Vector3.zero;
+            DesiredVelocity = ClosestDogPosition - gameObject.transform.position;
+            DesiredVelocity = Vector3.Normalize(DesiredVelocity);
+            DesiredVelocity *= speed;
+            Velocity = DesiredVelocity;
+
+            transform.forward = Vector3.Normalize(DesiredVelocity);
+        }
+
+        else
+        {
+            Velocity = Vector3.zero;
+       }
     }
 
     private bool IsAlive() {
